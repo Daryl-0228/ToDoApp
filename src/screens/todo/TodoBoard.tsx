f98d7095f2f4425a4c2562a6b5f37d14df5d2d0f
Daryl-0228 <s.shibano.0917@gmail.com>
2022-05-20 04:36:44 +0900

    作業反映(ログイン画面作成中)

---
+++ src/screens/todo/TodoBoard.tsx
@@ -0,0 +1,23 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Text, Button} from 'react-native-elements';
+
+export const TodoBoard: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text h1>Todo</Text>
+      <Button onPress={() => navigation.navigate('TodoForm')} title="ToDo登録" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
