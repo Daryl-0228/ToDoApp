dffa1ccc4fa96a5f45a58b01d97e8b7aa4f572f8
Daryl-0228 <shibano.0917@gmail.com>
2022-06-06 00:10:20 +0900

    [REST APIとの接続]項を実装

--- src/components/parts/todo/TodoList.tsx
+++ src/components/parts/todo/TodoList.tsx
@@ -5,13 +5,20 @@ import {Todo} from 'services';
 import {TodoItem} from './TodoItem';
 
 interface Props {
-  todos: Todo[];
+  todos?: Todo[];
+  processingTodos: number[];
   contentContainerStyle?: StyleProp<ViewStyle>;
   toggleTodoCompletion: (id: number) => void;
   removeTodo: (id: number) => void;
 }
 
-export const TodoList: React.FC<Props> = ({todos, contentContainerStyle, toggleTodoCompletion, removeTodo}) => {
+export const TodoList: React.FC<Props> = ({
+ todos,
+ contentContainerStyle,
+ toggleTodoCompletion,
+ removeTodo,
+ processingTodos,
+}) => {
   return (
     <FlatList
       style={styles.list}
@@ -24,6 +31,7 @@ export const TodoList: React.FC<Props> = ({todos, contentContainerStyle, toggleT
           text={todo.text}
           completed={todo.completed}
           toggleTodoCompletion={toggleTodoCompletion}
+          processing={processingTodos.includes(todo.id)}
         />
       )}
       keyExtractor={todo => String(todo.id)}
